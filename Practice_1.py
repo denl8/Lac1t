b422de52e2e84a323aa1f8eaf569dcc2322009df
denl8 <gtj550@gmail.com>
2021-04-05 16:57:59 +0300

    Create Practice_1.py

---
+++ Practice_1.py
@@ -0,0 +1,92 @@
+import math
+
+##0
+#print('Lacit')
+
+
+
+##1
+#print('Четверг')
+#print('Апрель')
+
+
+
+##2
+#i=0
+#while i<25:
+#    print(str(0)*(i+1))
+#    i+=1 
+
+
+
+##3
+#print('*     *     *')
+#print(' *   * *   *')
+#print('  * *   * *')
+#print('   *     *')
+
+
+
+##4
+#print('-------------\n| 4 | 9 | 2 |\n|-----------|\n| 3 | 5 | 7 |\n|-----------|\n| 8 | 1 | 6 |\n-------------')
+
+
+
+##5
+#print(1/2+1/4)
+
+
+
+##6
+#a,b=2,3
+#print((a+4*b)*(a-3*b)+a**2)
+
+
+
+##7
+#x=-2
+#print(abs(x)+x**5)
+
+
+
+##8
+#x=1.7
+#print((x+1)**2+3*(x+1))
+#x=3
+#print((x+1)**2+3*(x+1))
+
+
+
+##9
+#x=-2.34
+#print(((abs(x-5)-math.sin(x))/3)+math.sqrt(x**2+2014)*math.cos(2*x)-3)
+
+
+
+##10
+#x=3.6
+#print(math.exp(x-2)+abs(math.sin(x))-x**4*math.cos(1/x))
+
+
+
+##11
+#dict={'1':'зима', '2':'зима', '3':'весна', '4':'весна', '5':'весна', '6':'лето', '7':'лето', '8':'лето', '9':'осень', '10':'осень', '11':'осень', '12':'зима'}
+#print(dict[input()])
+
+
+
+##12
+#spisok=[1, 'lacit', True, 13.37]
+#print(spisok)
+#spisok.append('sayonara')
+#spisok.clear()
+#print(spisok)
+#spisok.reverse()
+#print(spisok)
+##создаем еще один список, т.к. предыдущий пустой
+#spisok=[1, 'lacit', True, 13.37]
+#i=0
+#while i<4:
+#    print(spisok[i])
+#    i+=1
+
